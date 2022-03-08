514da6ce32c988524aa644e6a5e65b14fbcdeb0e
Dobrovolskyi Anatolii <tolik@DESKTOP-911H15M.(none)>
2022-03-08 23:43:39 +0200

    [front] directory choice added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,9 @@ export function App() {
         <div>Prior to beginning the search</div>
         <div>Enter Spotify Playlist URL:</div>
       </header>
+      <header>
+      <div className='App-header-info'>If directory is not defined PL will be loaded into "userDownloads" folder</div>
+      </header>
     </div>
     
   );
@@ -19,12 +22,9 @@ export function App() {
 export function InputBar() {
   const [formData, updateFormData] = React.useState();
   const [playlist, updatePlaylist] = React.useState();
+  const [downloadPath, updateDownloadPath] = React.useState();
 
-  const handleChange = (e) => {
-    updateFormData(e.target.value.trim());
-  }
-
-  const handleSubmit = async (e) => {
+  const submitPlaylistLink = async (e) => {
     e.preventDefault();
     let response = await fetch("http://localhost:8080/api/v1/spotify/playlist?link=" + formData);
     let playlist = await response.json();
@@ -36,15 +36,22 @@ export function InputBar() {
       <div className="Bar">
         <div>
           <div className="SearchBar">
-            <form onSubmit={handleSubmit} class="inputForm">
-              <input type="text" name='PL-URL' required class="inputForm" onChange={handleChange}/>
+            <form onSubmit={submitPlaylistLink} class="inputForm">
+              <input type="text" name='PL-URL' required class="inputForm" onChange={
+                e => updateFormData(e.target.value.trim())}/>
               <input type="submit"/>
+            </form>
+          </div>
+          <div className="SearchBar">
+            <form class="inputForm">
+              <input type="text" name='DL-path' required class="inputForm" onChange={
+                e => updateDownloadPath(e.target.value)}/>
             </form>
           </div>
         </div>
       </div>
       { playlist &&
-        <PlaylistTable playlist={playlist}/>
+        <PlaylistTable playlist={playlist} downloadPath={downloadPath}/>
       }
     </>
   );
@@ -52,7 +59,7 @@ export function InputBar() {
 
 export default App;
 
-export function PlaylistTable({playlist}) {
+export function PlaylistTable({playlist, downloadPath}) {
   const [tracks, updateTracks] = React.useState(playlist.tracks.map(track => {
     return {...track,
       checked: true,
@@ -60,38 +67,26 @@ export function PlaylistTable({playlist}) {
     };
   }));
 
-  // function SendTrack(tracksToDownload) {
-  //   const HandleDownload = async (e) => {
-  //     e.preventDefault();
-  //     let DownloadResponce = await fetch("http://localhost:8080/api/v1/spotify/playlist?link=" + tracksToDownloadId.map(track));
-  //     if (DownloadResponce == "204") {
-
-  //       {React.setState.track.status: "Donwloading"}
-  //     }
-  //     else if (DownloadResponce == "401") {
-  //       {React.setState.track.status: "Not Authorized"}
-  //     }
-  //     else if (DownloadResponce == "404") {
-  //       {React.setState.track.status: "Wrong PL ID"}
-  //     }
-  //     else if (DownloadResponce == "429") {
-  //       {React.setState.track.status: "Too many Requests"}
-  //     }
-  //   }
-  // }
-
   return (
     <>
-      <button className='DonwloadButton' onClick={() => {
+      <button className='DownloadButton' onClick={() => {
           tracks.forEach(async (track, index) => {
             if (!track.checked) {
               return;
             }
-            const url = `http://localhost:8080/api/v1/download/start?id=${track.id}&path=./userDownloads/${`${track.artists} - ${track.title}`}.mp4`;
-            let downloadResponce = await fetch(url, {
+            let url = `http://localhost:8080/api/v1/download/start?id=${track.id}&path=`;
+            const title = `${track.artists} - ${track.title}`
+            console.log("downloadPath is", downloadPath);
+            if (downloadPath) {
+              url += `${downloadPath}/${title}.mp4`;
+            }
+            else {
+              url += `./userDownloads/${title}.mp4`;
+            }
+            let downloadResponse = await fetch(url, {
               method: 'POST'
             });
-            console.log(downloadResponce);
+            console.log(downloadResponse);
           })
         }
       }
